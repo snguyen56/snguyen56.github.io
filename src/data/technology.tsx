00006e0290f146a180b8d49d7270de85d06baeda
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-12-21 19:37:35 -0500

    add react and skills section

---
+++ src/data/technology.tsx
@@ -0,0 +1,16 @@
+import html5 from "../assets/icons/html5.svg";
+
+export const technologies = [
+  {
+    icon: html5,
+    url: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
+  },
+  {
+    icon: html5,
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    icon: html5,
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+];

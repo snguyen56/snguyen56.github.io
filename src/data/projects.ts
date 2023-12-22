37d510e7046b800dcef71010e5a2877620a01809
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-12-22 15:34:26 -0500

    code cleanup

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects = [
     title: "MERN Budget",
     image: MERNImage,
     description:
-      "A budget app created using the MERN stack (MongoDB, ExpressJS, React, NodeJS). The app allows you to set budgets and tracks income/expenses.",
+      "A fullstack budget app created using the MERN stack (MongoDB, ExpressJS, React, NodeJS). The app allows you to set budgets and tracks income/expenses.",
     github_url: "https://github.com/snguyen56/MERN-budget",
   },
 ];

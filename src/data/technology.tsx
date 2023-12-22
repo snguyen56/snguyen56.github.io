0f3059ed5b10ca7ac1c421d23e094c0cf00d353b
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-12-22 16:57:16 -0500

    added more skills

--- src/data/technology.tsx
+++ src/data/technology.tsx
@@ -1,6 +1,38 @@
 import html5 from "../assets/icons/html5.svg";
+import CSS from "../assets/icons/css3.svg";
+import javascript from "../assets/icons/javascript.svg";
+import typescript from "../assets/icons/typescript.svg";
+import python from "../assets/icons/python.svg";
+import C from "../assets/icons/c.svg";
+import express from "../assets/icons/express.svg";
+import mui from "../assets/icons/mui.svg";
+import tailwind from "../assets/icons/tailwindcss.svg";
 
 export const technologies = [
+  {
+    name: "python",
+    icon: python,
+    url: "https://www.python.org/",
+    alt: "python logo",
+  },
+  {
+    name: "C",
+    icon: C,
+    url: "https://www.open-std.org/jtc1/sc22/wg14/",
+    alt: "C logo",
+  },
+  {
+    name: "javascript",
+    icon: javascript,
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    alt: "javascript logo",
+  },
+  {
+    name: "typescript",
+    icon: typescript,
+    url: "https://www.typescriptlang.org/",
+    alt: "typescript logo",
+  },
   {
     name: "HTML",
     icon: html5,
@@ -8,15 +40,27 @@ export const technologies = [
     alt: "HTML logo",
   },
   {
-    name: "HTML",
-    icon: html5,
+    name: "CSS",
+    icon: CSS,
     url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    alt: "HTML logo",
+    alt: "CSS logo",
   },
   {
-    name: "HTML",
-    icon: html5,
-    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    alt: "HTML logo",
+    name: "mui",
+    icon: mui,
+    url: "https://mui.com/",
+    alt: "mui logo",
+  },
+  {
+    name: "tailwind",
+    icon: tailwind,
+    url: "https://tailwindcss.com/",
+    alt: "tailwind logo",
+  },
+  {
+    name: "express",
+    icon: express,
+    url: "https://expressjs.com/",
+    alt: "express logo",
   },
 ];

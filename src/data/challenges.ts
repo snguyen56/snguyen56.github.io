837acc700e8b7bcf2e799c4cf5bfa84b4a1caf92
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-12-22 16:02:25 -0500

    added project content

---
+++ src/data/challenges.ts
@@ -0,0 +1,20 @@
+import QRImage from "../assets/images/QRcode.png";
+import ResultsImage from "../assets/images/results summary.png";
+export const challenges = [
+  {
+    title: "Frontend Mentor - Results summary component solution",
+    image: ResultsImage,
+    description:
+      "This is a solution to the Results summary component challenge on Frontend Mentor.",
+    github_url: "https://github.com/snguyen56/order-summary-component-tailwind",
+    project_url: "https://snguyen56.github.io/WeatherPulse/",
+  },
+  {
+    title: "Frontend Mentor - QR code component solution",
+    image: QRImage,
+    description:
+      "This is a solution to the QR code component challenge on Frontend Mentor.",
+    github_url: "https://github.com/snguyen56/qr-code-react-tailwind",
+    project_url: "https://snguyen56.github.io/qr-code-react-tailwind/",
+  },
+];
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,26 @@
 import MERNImage from "../assets/images/MERN_budget.png";
+import recipesImage from "../assets/images/recipes.png";
+import weatherImage from "../assets/images/weatherpulse.png";
 
 export const projects = [
   {
-    title: "MERN Budget",
+    title: "WeatherPulse",
+    image: weatherImage,
+    description:
+      "A simple weather forecast app that uses the open source weather API Open-Meteo to gather global forecasting data and present it in a user friendly interface.",
+    github_url: "https://github.com/snguyen56/WeatherPulse",
+    project_url: "https://snguyen56.github.io/WeatherPulse/",
+  },
+  {
+    title: "NextJS Recipe Website",
+    image: recipesImage,
+    description:
+      "A recipe website using the NextJS framework (v12) for the frontend and the Contentful CMS (content management system) to store the recipe data.",
+    github_url: "https://github.com/snguyen56/recipes",
+    project_url: "https://recipes-umber.vercel.app/",
+  },
+  {
+    title: "MERN Budget Website",
     image: MERNImage,
     description:
       "A fullstack budget app created using the MERN stack (MongoDB, ExpressJS, React, NodeJS). The app allows you to set budgets and tracks income/expenses.",

245a6b650cc6f73209349c9290a3f82d67627808
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-12-30 19:26:26 -0500

    add skill color and improve its responsiveness

--- src/data/technology.tsx
+++ src/data/technology.tsx
@@ -1,12 +1,12 @@
-import html5 from "../assets/icons/html5.svg";
-import CSS from "../assets/icons/css3.svg";
-import javascript from "../assets/icons/javascript.svg";
-import typescript from "../assets/icons/typescript.svg";
-import python from "../assets/icons/python.svg";
-import C from "../assets/icons/c.svg";
-import express from "../assets/icons/express.svg";
-import mui from "../assets/icons/mui.svg";
-import tailwind from "../assets/icons/tailwindcss.svg";
+import html5 from "../assets/icons/html5.svg?raw";
+import CSS from "../assets/icons/css3.svg?raw";
+import javascript from "../assets/icons/javascript.svg?raw";
+import typescript from "../assets/icons/typescript.svg?raw";
+import python from "../assets/icons/python.svg?raw";
+import C from "../assets/icons/c.svg?raw";
+import express from "../assets/icons/express.svg?raw";
+import mui from "../assets/icons/mui.svg?raw";
+import tailwind from "../assets/icons/tailwindcss.svg?raw";
 
 export const technologies = [
   {
@@ -14,53 +14,62 @@ export const technologies = [
     icon: python,
     url: "https://www.python.org/",
     alt: "python logo",
+    color: "hover:fill-[#3776AB]",
   },
   {
     name: "C",
     icon: C,
     url: "https://www.open-std.org/jtc1/sc22/wg14/",
     alt: "C logo",
+    color: "hover:fill-[#A8B9CC]",
   },
   {
     name: "javascript",
     icon: javascript,
     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     alt: "javascript logo",
+    color: "hover:fill-[#F7DF1E]",
   },
   {
     name: "typescript",
     icon: typescript,
     url: "https://www.typescriptlang.org/",
     alt: "typescript logo",
+    color: "hover:fill-[#3178C6]",
   },
   {
     name: "HTML",
     icon: html5,
     url: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
     alt: "HTML logo",
+    color: "hover:fill-[#E34F26]",
   },
   {
     name: "CSS",
     icon: CSS,
     url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     alt: "CSS logo",
+    color: "hover:fill-[#1572B6]",
   },
   {
     name: "mui",
     icon: mui,
     url: "https://mui.com/",
     alt: "mui logo",
+    color: "hover:fill-[#007FFF]",
   },
   {
     name: "tailwind",
     icon: tailwind,
     url: "https://tailwindcss.com/",
     alt: "tailwind logo",
+    color: "hover:fill-[#06B6D4]",
   },
   {
     name: "express",
     icon: express,
     url: "https://expressjs.com/",
     alt: "express logo",
+    color: "hover:fill-[#000000]",
   },
 ];
